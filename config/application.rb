7e67fd44c804a5f8af2547d9467de22440803a9c
Rob <rob@ldg.net>
2012-10-21 20:17:43 -0400

    Adding config

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ end
 
 module Hephaestus
   class Application < Rails::Application
+
+    config.puppetlabs_forge_url = "http://forge.puppetlabs.com"
+    config.local_releases_path = "#{Rails.root}/public/system/releases"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
